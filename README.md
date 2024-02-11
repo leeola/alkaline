679a4ae619a1246bb827d4fba42bf9732a51808e
Lee Olayvar <leegit@fastmail.com>
2024-02-11 13:26:37 -0800

    feat: explore primary adapter interface

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Alkaline
+
+**Development Status**: Exploration / Prototyping
--- alkaline/src/lib.rs
+++ alkaline/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod error {
+    pub type Result<T, E = Error> = std::result::Result<T, E>;
+    #[derive(Debug, thiserror::Error)]
+    pub enum Error {
+        #[error(transparent)]
+        Other(anyhow::Error),
+    }
 }
+pub mod value {
+    /// Early limited dynamic value type.
+    pub enum Value {
+        Number(Number),
+    }
+    pub enum Number {
+        Integer(i64),
+        Float(f64),
+    }
+    pub struct Map;
+}
+pub mod schema {
+    pub enum Schema {}
+}
+pub mod adapter {
+    use crate::{
+        error::Result,
+        schema::Schema,
+        value::{Map, Value},
+    };
+    use async_trait::async_trait;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    #[async_trait]
+    pub trait Adapter: Sized {
+        async fn init(config: &Map) -> Self;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        async fn read(&self, rows_buf: &mut Vec<Value>, query: &()) -> Result<()>;
+
+        // TODO: impl.
+        // async fn create();
+        // async fn update();
+        // async fn delete();
+
+        // NIT: Probably better to return a paths of what is mutable?
+        async fn mutable(&self) -> bool {
+            false
+        }
+        async fn schema(&self) -> Option<Schema> {
+            None
+        }
+    }
+}
+pub mod query {
+    use crate::value::Value;
+
+    pub struct Query {
+        selection: Value,
+        filters: (),
     }
 }
