0c62d813e0d5034e5a7335bb3dcad5496b6ac51c
Lee Olayvar <leegit@fastmail.com>
2024-02-14 12:49:03 -0800

    feat: tracing setup for alk

--- alk/src/main.rs
+++ alk/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use tracing::{info, metadata::LevelFilter, subscriber};
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() {
+    // TODO: Move logging init to a core utility, for ease of test setup.
+    subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .with_env_var("RUST_LOG")
+                    .from_env_lossy(),
+            )
+            .finish(),
+    )
+    .unwrap();
+
+    info!("Hello, world!");
 }
