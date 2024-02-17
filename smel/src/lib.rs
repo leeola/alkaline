703609ce81b5a17c1374716a84666c844a9c3e6f
Lee Olayvar <leegit@fastmail.com>
2024-02-17 14:47:21 -0800

    feat(smel): add smel

---
+++ smel/src/lib.rs
@@ -0,0 +1,11 @@
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+#[derive(Debug, thiserror::Error)]
+pub enum Error {}
+
+#[derive(Clone)]
+pub struct Smel {}
+impl Smel {
+    pub fn new<S: Into<String>>(_s: S) -> Result<Self> {
+        todo!()
+    }
+}
