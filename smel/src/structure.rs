30d44248dedad5c3d5f77c93f3aed74bd3777720
Lee Olayvar <leegit@fastmail.com>
2024-02-27 16:59:06 -0800

    wip, stash editing

--- smel/src/structure.rs
+++ smel/src/structure.rs
@@ -28,3 +28,43 @@ pub enum Structure {
 impl Structure {}
 
 pub struct Value;
+
+#[cfg(test)]
+pub mod test {
+    use comrak::{
+        nodes::{AstNode, NodeValue},
+        Arena, ComrakOptions,
+    };
+
+    #[test]
+    fn exploring_comrak() {
+        let input = r#"
+            # foo
+            bar baz
+            ## Bang
+        "#;
+        let arena = Arena::new();
+        let root = comrak::parse_document(&arena, input, &ComrakOptions::default());
+        modify_headers_in_ast(root);
+        let mut md = Vec::<u8>::new();
+        comrak::format_commonmark(root, &ComrakOptions::default(), &mut md).unwrap();
+        let md = String::from_utf8(md).unwrap();
+        dbg!(md);
+    }
+    fn modify_headers_in_ast<'a>(node: &'a AstNode<'a>) {
+        for child in node.children() {
+            #[allow(clippy::single_match)]
+            match &mut child.data.borrow_mut().value {
+                NodeValue::Heading(heading) => {
+                    dbg!("got a heading");
+                    // Prepend and append quotes to the heading's content
+                    heading.level = 4; // Example modification, adjust as needed
+                },
+                _ => {
+                    dbg!("not a heading?");
+                },
+            }
+            modify_headers_in_ast(child);
+        }
+    }
+}
