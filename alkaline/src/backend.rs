5ee2b49c7c64dd7923fe997cf09ac216826d68e0
Lee Olayvar <leegit@fastmail.com>
2024-06-23 12:56:47 -0700

    refactor(alkaline): remove local backend
    It'll come back eventually, but atm there's no use.

--- alkaline/src/backend.rs
+++ alkaline/src/backend.rs
@@ -8,43 +8,6 @@ use std::ops::{Deref, DerefMut};
 // pub mod conn {}
 pub mod ephemeral_reference;
 pub mod persistent_reference;
-pub mod local {
-    use super::{Connection, Registry};
-    use crate::{adapter, error::Result};
-    use async_trait::async_trait;
-
-    pub struct Local<S> {
-        #[allow(unused)]
-        storage: S,
-    }
-    impl<S> Local<S> {
-        pub fn new(storage: S) -> Self {
-            Self { storage }
-        }
-    }
-    #[cfg(test)]
-    impl Local<crate::storage::memory::MemoryDb> {
-        pub fn test() -> Self {
-            #[allow(clippy::default_constructed_unit_structs)]
-            Self::new(crate::storage::memory::MemoryDb::default())
-        }
-    }
-    #[async_trait]
-    impl<S> Registry for Local<S> {
-        fn register<T: adapter::Init>(&mut self) {
-            todo!()
-        }
-    }
-    #[async_trait]
-    impl<S> Connection for Local<S>
-    where
-        S: Send + Sync,
-    {
-        async fn databases(&self) -> Result<Vec<String>> {
-            todo!()
-        }
-    }
-}
 
 pub trait Registry {
     fn register<T: adapter::Init>(&mut self);
